fe3f22461da1650086c7ba75d96c29e9cbfa8ee9
Aliaksei Siankevich <andfly@tut.by>
2021-09-15 13:29:51 +0300

    TD-2: added layout

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,59 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="container">
+      <div class="todo-app">
+        <h1 class="todo-app-header">to do list</h1>
+        <div class="todo-app-list">
+          <div
+            class="todo-app-list-task"
+            v-for="(task, index) in tasks"
+            :key="index"
+          >
+            <div class="left-side-task">
+              <input
+                type="checkbox"
+                class="custom-checkbox"
+                :id="task.id"
+                name="task"
+              />
+              <label :for="task.id">{{ task.title }}</label>
+            </div>
+            <button class="delete-task-btn"></button>
+          </div>
+          <input class="input-add-new-task" placeholder="Add a new task" />
+        </div>
+        <div class="todo-app-sort">
+          <div class="left-side side">
+            <div class="todo-app-sort-checked-count">1/3 left</div>
+          </div>
+          <div class="right-side side">
+            <div class="todo-app-sort-all-tasks sort-btn">All</div>
+            <div class="todo-app-sort-active-tasks sort-btn">Active</div>
+            <div class="todo-app-sort-completed-tasks sort-btn">Completed</div>
+          </div>
+        </div>
+      </div>
+      <div class="img-list"></div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      tasks: [
+        { title: "Task 1", checked: false, id: 1 },
+        { title: "Task 2", checked: false, id: 2 },
+        { title: "Task 3", checked: false, id: 3 },
+      ],
+    };
   },
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import url("https://fonts.googleapis.com/css2?family=Inter&family=Open+Sans:wght@600&display=swap");
+@import "./assets/index.scss";
 </style>
